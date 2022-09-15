9c0d7cd2869b74a89b72b9fc9704ccac3c3b3e28
Samuel <samuelsoutosilva14@gmail.com>
2022-09-15 17:26:08 -0300

    fix: add vehicle attributes to car and export sch

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -2,6 +2,11 @@ import { z } from 'zod';
 import { IVehicle } from './IVehicle';
 
 const carZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().int().min(1900).max(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
   doorsQty: z.number().int().gte(2).lte(4),
   seatsQty: z.number().int().gte(2).lte(7),
 });
@@ -9,3 +14,5 @@ const carZodSchema = z.object({
 type Car = z.infer<typeof carZodSchema>;
 
 export interface ICar extends IVehicle, Car { }
+
+export { carZodSchema };
