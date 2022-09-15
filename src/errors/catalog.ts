11e1260fb9e6c482751dc778892eac9ed2f41ade
Samuel <samuelsoutosilva14@gmail.com>
2022-09-15 19:02:09 -0300

    fix: remove EntityNotFound from catalog

--- src/errors/catalog.ts
+++ src/errors/catalog.ts
@@ -1,5 +1,4 @@
 export enum ErrorTypes {
-  EntityNotFound = 'EntityNotFound',
   InvalidMongoId = 'InvalidMongoId',
   InvalidPostParam = 'InvalidPostParam',
 }
@@ -14,10 +13,6 @@ export type ErrorCatalog = {
 };
 
 export const errorCatalog: ErrorCatalog = {
-  EntityNotFound: {
-    message: 'Entity not found',
-    httpStatus: 404,
-  },
   InvalidMongoId: {
     message: 'Id must be a 24 characters hexadecimal',
     httpStatus: 400,
