e5cfa0c75665b1f4b88fae0702580dddafc63632
Samuel <samuelsoutosilva14@gmail.com>
2022-09-15 17:06:57 -0300

    feat: add errorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middleware/errorHandler';
 
 const app = express();
+app.use(express.json());
+app.use(errorHandler);
 
 export default app;
