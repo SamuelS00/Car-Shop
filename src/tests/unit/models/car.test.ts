9ecee8fea11bff9bda0bf6bbd65e9f1fd9a97e2c
Samuel <samuelsoutosilva14@gmail.com>
2022-09-16 13:24:38 -0300

    tests: add tests of the car model later

--- src/tests/unit/models/car.test.ts
+++ src/tests/unit/models/car.test.ts
@@ -3,6 +3,7 @@ import sinon from 'sinon';
 import CarModel from '../../../models/Car';
 import { Model } from 'mongoose';
 import { carMock, carMockWithId } from '../../mocks/carMock';
+import CarService from '../../../services/Car';
 
 describe('Car Model', () => {
   const carModel = new CarModel();
@@ -10,6 +11,7 @@ describe('Car Model', () => {
   before(() => {
     sinon.stub(Model, 'create').resolves(carMockWithId);
     sinon.stub(Model, 'find').resolves([carMock]);
+    sinon.stub(Model, 'findOne').resolves(carMockWithId);
   });
 
   after(() => {
@@ -26,7 +28,22 @@ describe('Car Model', () => {
   describe('returns all cars created', () => {
     it('successfully returned', async () => {
       const cars = await carModel.read();
-      expect(cars).to.be.deep.equal(cars)
+      expect(cars).to.be.deep.equal([carMock])
+    });
+  });
+
+  describe('returns car from specified id successfully ', () => {
+    it('successfully found', async () => {
+      const car = await carModel.readOne(carMockWithId._id);
+      expect(car).to.be.deep.equal(carMockWithId);
+    });
+
+    it('_id not found', async () => {
+      try {
+				await carModel.readOne('iderrado');
+			} catch (error: any) {
+				expect(error.message).to.be.equal('InvalidMongoId');
+			}
     });
   });
 });
