bc3db8afebff279db8663bdd333730ab51eda0d3
Samuel <samuelsoutosilva14@gmail.com>
2022-09-16 15:04:27 -0300

    feat(service): add method update in service layer

--- src/services/Car.ts
+++ src/services/Car.ts
@@ -27,6 +27,14 @@ class CarService implements IService<ICar> {
     if (!car) throw new Error(ErrorTypes.ObjectNotFound);
     return car;
   }
+
+  public async update(_id: string, obj: unknown): Promise<ICar> {
+    const parsed = carZodSchema.safeParse(obj);
+    if (!parsed.success) throw parsed.error;
+    const car = await this._car.update(_id, parsed.data);
+    if (!car) throw new Error(ErrorTypes.ObjectNotFound);
+    return car;
+  }
 }
 
 export default CarService;
