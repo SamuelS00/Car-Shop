6743e2dadde51e985f1c27cc71bf30986f433daf
Samuel <samuelsoutosilva14@gmail.com>
2022-09-16 17:02:19 -0300

    fix: fix parameters for partial generic

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -1,6 +1,6 @@
 export interface IService<T> {
-  create(obj: unknown): Promise<T>,
+  create(obj: Partial<T>): Promise<T>,
   read(): Promise<T[]>,
   readOne(_id: string): Promise<T>,
-  update(_id: string, obj: unknown):Promise<T>
+  update(_id: string, obj: Partial<T>):Promise<T>,
 }
