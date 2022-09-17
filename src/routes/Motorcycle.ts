0204aa538e8f1f55ecdee55223cbe0fa8c1aad6f
Samuel <samuelsoutosilva14@gmail.com>
2022-09-17 17:54:40 -0300

    feat(motorcycle): add route POST for cread method

---
+++ src/routes/Motorcycle.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import MotorcycleController from '../controllers/Motorcycle';
+import MotorcycleService from '../services/Motorcycle';
+import MotorcycleModel from '../models/Motorcycle';
+
+const route = Router();
+
+const motorcycleModel = new MotorcycleModel();
+const motorcycleService = new MotorcycleService(motorcycleModel);
+const motorcycleController = new MotorcycleController(motorcycleService);
+
+route.post('/motorcycles', (req, res) => motorcycleController.create(req, res));
