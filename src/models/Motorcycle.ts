ad4ae86e060f2a8c0500dc902cdcad2da57e2805
Samuel <samuelsoutosilva14@gmail.com>
2022-09-17 17:32:57 -0300

    feat(models): add Motorcycle model

---
+++ src/models/Motorcycle.ts
@@ -0,0 +1,23 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { IMotorcycle } from '../interfaces/IMotorcycle';
+import MongoModel from './MongoModel';
+
+const motorcycleMongooseSchema = new Schema<IMotorcycle>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  category: { type: String, enum: ['Street', 'Custom', 'Trail'] },
+  engineCapacity: Number,
+}, {
+  versionKey: false,
+});
+
+class Motorcycle extends MongoModel<IMotorcycle> {
+  constructor(model = mongooseCreateModel('Car', motorcycleMongooseSchema)) {
+    super(model);
+  }
+}
+
+export default Motorcycle;
