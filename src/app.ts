5e024c35912ab05baef7e7270aae2d98ff89feb3
Samuel <samuelsoutosilva14@gmail.com>
2022-10-17 11:45:13 -0300

    feat: add config swagger in app

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import express from 'express';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './docs/swagger_output.json';
 import errorHandler from './middleware/errorHandler';
 import carRouter from './routes/Car';
 import motorcycleRouter from './routes/Motorcycle';
 
+/** Middleware */
 const app = express();
 app.use(express.json());
-app.use(carRouter);
-app.use(motorcycleRouter);
+
+/* Swagger */
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(errorHandler);
+
+/** Routes */
+app.use('/cars', carRouter);
+app.use('/motorcycles', motorcycleRouter);
 
 export default app;
